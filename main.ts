1de92273a1b2d76493406a4105cdaabe5091f919
Danila <danilavaganov41@gmail.com>
2024-11-01 17:55:30 +0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,14 +3,14 @@ function func(a, b, c){
         return 'ошибка'
     }
     else{
-        let discriminant = b * b - 4 * a * c
+        const discriminant = b * b - 4 * a * c
         if (discriminant < 0) {
             return 'Уравнение не имеет реальных корней';
         } else if (discriminant === 0) {
             return 'Уравнение имеет один корень: ' + (-b / (2 * a));
         } else {
-            var root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            var root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
             return 'Уравнение имеет два корня: ' + root1 + ' и ' + root2;
         }
     }
