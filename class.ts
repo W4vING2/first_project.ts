6d79339260702dc460ac7ed066cc5ac29b8c9eba
Danila <danilavaganov41@gmail.com>
2024-11-06 21:12:27 +0600

    Add files via upload

---
+++ class.ts
@@ -0,0 +1,41 @@
+class Book{
+    title: string
+    author: string
+    year: number
+
+    constructor(title:string, author:string, year:number){
+        this.title = title
+        this.author = author
+        this.year = year
+    }
+
+    Getinfo() {
+        console.log(`Название: ${this.title}, Автор: ${this.author}, Год: ${this.year}`)
+    }
+}
+const bookd = new Book('гарри поттер', 'не знаю', 455);
+bookd.Getinfo()
+
+const first = new Book('впппк', 'праопапр', 52)
+const second = new Book('gege wepe', 'праопаdaadadadadр', 51)
+let MyBooks = [first, second]
+
+class Library{
+    books: Array<Book>;
+
+    constructor(books: Array<Book>){
+        this.books = books
+    }
+    addBook(book:Book){
+        this.books.push(book)
+    }
+    listBooks(){
+        for(const list of MyBooks){
+            list.Getinfo()
+        }
+    }
+}
+
+
+
+
